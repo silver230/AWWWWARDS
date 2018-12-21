c2d370269a437ad3d6d69ddb7f51acd4cadbee82
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-21 13:18:48 +0300

    configurred index html to work

--- award/views.py
+++ award/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
 # Create your views here.
+def index(request):
+  date = dt.date.today()
+
+  return render(request,'index.html',{"date":date})
+
