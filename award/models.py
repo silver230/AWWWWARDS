e7164cb2888f6f1793fc3016c59414dd01fc2382
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-22 19:40:54 +0300

    created profile function with all the fields into it

--- award/models.py
+++ award/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+ 
 
 # Create your models here.
+
+class Profile (models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    username = models.CharField(max_length= 70)
+    bio = models.CharField(max_length=60),
+    date_of_birth = models.TimeField(max_length=30),
+    
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+   
+
+    def __str__(self):
+        return self.username
--- award/views.py
+++ award/views.py
@@ -7,3 +7,8 @@ def index(request):
 
   return render(request,'index.html',{"date":date})
 
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    user = User.objects.get(username=request.user)
+    return render(request, 'profile/profile.html', {'user': current_user, "profile": profile})
