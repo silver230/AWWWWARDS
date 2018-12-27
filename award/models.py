60ad1b9ce2d408c0df230a0205e8570e7e091a66
sylvester omondi <sylvesteromondi49@gmail.com>
2018-12-27 19:53:24 +0300

    created projects class with all the fields needed

--- award/models.py
+++ award/models.py
@@ -11,7 +11,7 @@ class Profile (models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True),
     user_name = models.CharField(max_length= 70),
     bio = models.CharField(max_length=60),
-
+    profile_pic = models.ImageField(upload_to='media/', blank=True)
 
     @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
@@ -32,3 +32,15 @@ class Profile (models.Model):
     def __str__(self):
         return self.user_name
 
+class Project(models.Model):
+
+    name = models.CharField(max_length=30)
+    photo =  models.CharField(max_length=50)
+    project_description = models.CharField(max_length=50)
+    project_url = models.CharField(max_length=50)
+    technologies_used = models.CharField(max_length=50)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    posted_time = models.DateTimeField(auto_now_add=True,)
