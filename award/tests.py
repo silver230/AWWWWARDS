d5b85466db9c5d16760c516c2a9637ef968ce542
sylvester omondi <sylvesteromondi49@gmail.com>
2019-01-09 13:38:57 +0300

    created tests

--- award/tests.py
+++ award/tests.py
@@ -1,25 +1,49 @@
 from django.test import TestCase
-from . models import Project        
+from . models import *
+from django.contrib.auth.models import User   
 
 # Create your tests here.
+class RatingTestClass(TestCase):
+    def setUp(self):
+        self.rating=Rating(comment="wooow",design=4,usability= 6, content=7)
+
+    # def tearDown(self) :
+    #     Rating.objects.all().delete()
+
+    def test_instance(self):
+        self.assertIsInstance(self.rating,Rating)
+
+    def test_save_rating(self):
+        self.assertFalse(self.rating in Rating.objects.all())
+        self.rating.save()
+        self.assertTrue(self.rating in Rating.objects.all())
+        self.rating.delete()
+
+    def test_delete_rating(self):
+        self.assertFalse(self.rating in Rating.objects.all())
+        self.rating.save()
+        self.assertTrue(self.rating in Rating.objects.all())
+        self.rating.delete()        
+
 class ProjectTestClass(TestCase):
     def setUp(self):
         self.projectTest=Project(name="blog",photo="gifts/media/images/blog.jpg",project_description="a better experience", project_url="https://moringaschool.instructure.com", technologies_used = "html")
 
-    # def tearDown(self) :
-    #     User.objects.all().delete()
+    def tearDown(self) :
+        User.objects.all().delete()
 
     def test_instance(self):
         self.assertIsInstance(self.projectTest,Project)
 
     def test_save_project(self):
-        self.assertFalse(self.projectTest in Project.objects.all())
-        self.projectTest.save()
         self.assertTrue(self.projectTest in Project.objects.all())
-        self.projectTest.delete()
+        self.projectTest.save()
+         
 
     def test_delete_project(self):
         self.assertFalse(self.projectTest in Project.objects.all())
         self.projectTest.save()
         self.assertTrue(self.projectTest in Project.objects.all())
         self.projectTest.delete()
+
+
